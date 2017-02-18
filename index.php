9832d2f14dd2cbb30c6cfa9fbfa37e521edf8d56
vaisanenjaska <vaisanenjaska@gmail.com>
2017-02-19 00:00:53 +0200

    asd

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $username = $_POST["username"];
 <script language="javascript" type="text/javascript">
 $(document).ready(function(){
 
-	var wsUri = "ws://language-exchange-cafe-chat.herokuapp.com/server:443.php";
+	var wsUri = "ws://language-exchange-cafe-chat.herokuapp.com/server.php:443";
 	websocket = new WebSocket(wsUri);
 
 	websocket.onopen = function(ev) {}
