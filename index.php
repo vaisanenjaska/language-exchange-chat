0198c3709c857a60895864c90d7d224ab15c5e8a
vaisanenjaska <vaisanenjaska@gmail.com>
2017-02-18 23:16:11 +0200

    asd

--- index.php
+++ index.php
@@ -1,117 +1,187 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style type="text/css">
+
+.panel{
+
+
+margin-right: 3px;
+}
+
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+	margin-right: 30%;
+	margin-left: 30%;
+    text-decoration: none;
+    display: block;
+    font-size: 16px;
+    cursor: pointer;
+	width:30%;
+    height:40px;
+	margin-top: 5px;
+
+}
+input[type=text]{
+		width:100%;
+		margin-top:5px;
+
+	}
+
+
+.chat_wrapper {
+	width: 70%;
+	height:472px;
+	margin-right: auto;
+	margin-left: auto;
+	background: #3B5998;
+	border: 1px solid #999999;
+	padding: 10px;
+	font: 14px 'lucida grande',tahoma,verdana,arial,sans-serif;
+}
+.chat_wrapper .message_box {
+	background: #F7F7F7;
+	height:350px;
+		overflow: auto;
+	padding: 10px 10px 20px 10px;
+	border: 1px solid #999999;
+}
+.chat_wrapper  input{
+	//padding: 2px 2px 2px 5px;
+}
+.system_msg{color: #BDBDBD;font-style: italic;}
+.user_name{font-weight:bold;}
+.user_message{color: #88B6E0;}
+
+@media only screen and (max-width: 720px) {
+    .chat_wrapper {
+        width: 95%;
+	height: 40%;
+	}
+
+
+	.button{ width:100%;
+	margin-right:auto;
+	margin-left:auto;
+	height:40px;}
+
+
+
+
+
+
+}
+
+</style>
+</head>
+<body>
 <?php
-include("config.php");
-$host = 'http://language-exchange-cafe-chat.herokuapp.com';
-$port = '9000';
-$null = NULL;
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($socket, 0, $port);
-socket_listen($socket);
-$clients = array($socket);
-while (true) {
-	$changed = $clients;
-	socket_select($changed, $null, $null, 0, 10);
-	if (in_array($socket, $changed)) {
-		$socket_new = socket_accept($socket);
-		$clients[] = $socket_new;
-		$header = socket_read($socket_new, 1024);
-		perform_handshaking($header, $socket_new, $host, $port);
-		$found_socket = array_search($socket, $changed);
-		unset($changed[$found_socket]);
-	}
-	foreach ($changed as $changed_socket) {
-		while(socket_recv($changed_socket, $buf, 1024, 0) >= 1)
+include("../database/config.php");
+$target = $_POST["chatTarget"];
+$username = $_POST["username"];
+?>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+
+<script language="javascript" type="text/javascript">
+$(document).ready(function(){
+
+	var wsUri = "ws://http://language-exchange-cafe-chat.herokuapp.com:9000/server.php";
+	websocket = new WebSocket(wsUri);
+
+	websocket.onopen = function(ev) {}
+
+	$('#send-btn').click(function(){
+		var mymessage = $('#message').val();
+		var myname = "<?php echo $username ?>";
+		var target = <?php echo $target ?>;
+
+		if(mymessage == ""){
+			alert("Enter Some message Please!");
+			return;
+		}
+
+		var objDiv = document.getElementById("message_box");
+		objDiv.scrollTop = objDiv.scrollHeight;
+
+		var msg = {
+		message: mymessage,
+		name: myname,
+		target: target
+		};
+
+		websocket.send(JSON.stringify(msg));
+	});
+
+	websocket.onmessage = function(ev) {
+		var msg = JSON.parse(ev.data);
+		var type = msg.type;
+		var umsg = msg.message;
+		var uname = msg.name;
+
+		if(type == 'usermsg' && umsg != null)
 		{
-			$received_text = unmask($buf);
-			$tst_msg = json_decode($received_text);
-			$user_name = $tst_msg->name;
-			$user_message = $tst_msg->message;
-			$target = $tst_msg->target;
-			$response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>$user_name, 'message'=>$user_message)));
-			send_message($response_text);
-			$username = mysqli_escape_string($db,$user_name);
-			$target_user = mysqli_escape_string($db,$target);
-			$message  = mysqli_escape_string($db,$user_message);
-			$id_sql = "Select user_id from User where name = '$username'";
-			$userquery = mysqli_query($db, $id_sql);
-			$row = mysqli_fetch_array($userquery, MYSQLI_ASSOC);
-			$user = $row['user_id'];
-			$user_id = mysqli_escape_string($db, $user);
-			$sql = "INSERT INTO Message(user_id,text,target_user) VALUES('$user_id','$message','$target_user')";
-			if(!empty($user_id)) {
-				$result = mysqli_query($db,$sql);
-			}
-			break 2;
+			$('#message_box').append("<div><span class=\"user_name\">"+uname+"</span> : <span class=\"user_message\">"+umsg+"</span></div>");
 		}
-		$buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-		if ($buf === false) {
-			$found_socket = array_search($changed_socket, $clients);
-			socket_getpeername($changed_socket, $ip);
-			unset($clients[$found_socket]);
-		}
-	}
-}
-socket_close($socket);
-function send_message($msg)
-{
-	global $clients;
-	foreach($clients as $changed_socket)
-	{
-		@socket_write($changed_socket,$msg,strlen($msg));
-	}
-	return true;
-}
-function unmask($text) {
-	$length = ord($text[1]) & 127;
-	if($length == 126) {
-		$masks = substr($text, 4, 4);
-		$data = substr($text, 8);
-	}
-	elseif($length == 127) {
-		$masks = substr($text, 10, 4);
-		$data = substr($text, 14);
-	}
-	else {
-		$masks = substr($text, 2, 4);
-		$data = substr($text, 6);
-	}
-	$text = "";
-	for ($i = 0; $i < strlen($data); ++$i) {
-		$text .= $data[$i] ^ $masks[$i%4];
-	}
-	return $text;
-}
-function mask($text)
-{
-	$b1 = 0x80 | (0x1 & 0x0f);
-	$length = strlen($text);
-	if($length <= 125)
-		$header = pack('CC', $b1, $length);
-	elseif($length > 125 && $length < 65536)
-		$header = pack('CCn', $b1, 126, $length);
-	elseif($length >= 65536)
-		$header = pack('CCNN', $b1, 127, $length);
-	return $header.$text;
-}
-function perform_handshaking($receved_header,$client_conn, $host, $port)
-{
-	$headers = array();
-	$lines = preg_split("/\r\n/", $receved_header);
-	foreach($lines as $line)
-	{
-		$line = chop($line);
-		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
+		if(type == 'system')
 		{
-			$headers[$matches[1]] = $matches[2];
+			$('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
 		}
-	}
-	$secKey = $headers['Sec-WebSocket-Key'];
-	$secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-	$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-	"Upgrade: websocket\r\n" .
-	"Connection: Upgrade\r\n" .
-	"WebSocket-Origin: $host\r\n" .
-	"WebSocket-Location: ws://$host:$port/demo/shout.php\r\n".
-	"Sec-WebSocket-Accept:$secAccept\r\n\r\n";
-	socket_write($client_conn,$upgrade,strlen($upgrade));
-}
+
+		$('#message').val('');
+
+		var objDiv = document.getElementById("message_box");
+		objDiv.scrollTop = objDiv.scrollHeight;
+	};
+
+	websocket.onerror	= function(ev){$('#message_box').append("<div class=\"system_error\">Error Occurred - "+ev.data+"</div>");};
+	websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");};
+});
+
+
+
+
+</script>
+<div class="chat_wrapper">
+<div class="message_box" id="message_box">
+	<?php
+		$user_name = mysqli_escape_string($db,$username);
+		$target_user = mysqli_escape_string($db,$target);
+		$id_sql = "Select user_id from User where name = '$user_name'";
+		$userquery = mysqli_query($db, $id_sql);
+		$row = mysqli_fetch_array($userquery, MYSQLI_ASSOC);
+		$user = $row['user_id'];
+		$user_id = mysqli_escape_string($db, $user);
+		$sql = "Select user_id, text, target_user from Message where user_id = $user_id and target_user = $target_user or user_id = $target_user and target_user = $user_id order by date";
+		$result = mysqli_query($db,$sql);
+		while ($message_row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+			$id = mysqli_escape_string($db,$message_row['user_id']);
+			$name_sql = "Select name from User where user_id = '$id'";
+			$userquery = mysqli_query($db, $name_sql);
+			$user_row = mysqli_fetch_array($userquery, MYSQLI_ASSOC);
+			$user = $user_row['name'];
+			echo "<div><span class=\"user_name\">".$user."</span> : <span class=\"user_message\">".$message_row['text']."</span></div>";
+		}
+	?>
+</div>
+<div class="panel">
+<input type="text" name="message" id="message" placeholder="Message" maxlength="80"
+onkeydown = "if (event.keyCode == 13)document.getElementById('send-btn').click()"  />
+
+
+
+
+
+</div>
+
+<button id="send-btn" class=button>Send</button>
+
+</div>
+
+</body>
+</html>
