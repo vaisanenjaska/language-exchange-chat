4a5f7d4371d7070f933ed6dcca0c135ef2300076
vaisanenjaska <vaisanenjaska@gmail.com>
2017-02-19 18:09:03 +0200

    asd

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
 var mysql = require('mysql');
 
-exports.connection = mysql.createConnection(
-    {
-      host     : process.env.MYSQL_DB.toString(),
-      user     : process.env.MYSQL_HOST.toString(),
-      password : process.env.MYSQL_PASS.toString(),
-      database : process.env.MYSQL_USER.toString(),
-    }
-);
+exports.connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
