36dcff890e38e760d63c7dca923df53650ed162f
vaisanenjaska <vaisanenjaska@gmail.com>
2017-02-19 17:48:00 +0200

    changed index.php

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 exports.connection = mysql.createConnection(
     {
-      host     : 'process.env.MYSQL_DB',
-      user     : 'process.env.MYSQL_HOST',
-      password : 'process.env.MYSQL_PASS',
-      database : 'process.env.MYSQL_USER',
+      host     : process.env.MYSQL_DB,
+      user     : process.env.MYSQL_HOST,
+      password : process.env.MYSQL_PASS,
+      database : process.env.MYSQL_USER,
     }
 );
